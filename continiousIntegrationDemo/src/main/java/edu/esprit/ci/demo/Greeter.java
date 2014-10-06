529ebaf828e0ecb8f68c5d5b1abea295a8f78e43
wael.aniba <wael.aniba@esprit.tn>
2014-10-06 16:20:25 +0200

    my first commit
    Signed-off-by: wael.aniba <wael.aniba@esprit.tn>

---
+++ continiousIntegrationDemo/src/main/java/edu/esprit/ci/demo/Greeter.java
@@ -0,0 +1,10 @@
+package edu.esprit.ci.demo;
+
+public class Greeter {
+	
+	public String sayHello(String name){
+		
+		return "Hello " + name; 
+	}
+
+}
---
+++ continiousIntegrationDemo/src/test/java/edu/esprit/ci/demo/GreeterTest.java
@@ -0,0 +1,25 @@
+package edu.esprit.ci.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GreeterTest {
+
+	Greeter greeter;
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		greeter = new Greeter();
+	}
+
+	@Test
+	public void test() {
+		
+		assertEquals("Hello Kais", greeter.sayHello("Kais"));
+		
+	}
+
+}
