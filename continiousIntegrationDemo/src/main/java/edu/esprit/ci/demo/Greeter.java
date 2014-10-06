889e9eb2df359199301e653ed14c62008c496ac8
wael.aniba <wael.aniba@esprit.tn>
2014-10-06 16:20:25 +0200

    my first commit
    Signed-off-by: wael.aniba <wael.aniba@esprit.tn>

---
+++ continiousIntegrationDemo/src/main/java/edu/esprit/ci/demo/Greeter.java
@@ -0,0 +1,15 @@
+package edu.esprit.ci.demo;
+
+public class Greeter {
+	
+	public String sayHello(String name){
+		
+		return "Hello " + name; 
+	}
+	
+	public String sayGoodBye(String name){
+		
+		return "GoodBye " + name; 
+	}
+
+}
